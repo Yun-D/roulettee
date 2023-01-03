b4001622c2e46b9ed550c308025437031a000d5d
Yun-D <yund_272@naver.com>
2023-01-03 16:58:14 +0900

    ✨Feat: googleChart import, 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+import { Chart } from "react-google-charts";
 
-import { ADD_ITEM } from '../store';
-import Item from '../components/Item';
+import { ADD_ITEM } from "../store";
+import Item from "../components/Item";
 
-function App( {items, addItem} ) {
+function App({ items, addItem }) {
   const [text, setText] = useState("");
 
   function plusItem(e) {
@@ -17,32 +19,76 @@ function App( {items, addItem} ) {
     setText("");
   }
 
+  const chartOptions = {
+    legend: "none", //항목 데이터 알려주는 부분
+    pieSliceText: "none", //항목이 차지하는 비율
+    //pieStartAngle: 135,
+    //tooltip: { trigger: "none" }, //마우스오버 툴팁
+    slices: {
+      //색상 지정할 수 있음
+      0: { color: "skyblue" },
+      1: { color: "purple" },
+      2: { color: "yellow" },
+      3: { color: "beige" },
+    },
+  };
+
   return (
     <>
-      <h1>Roulettee!</h1>
+      <div>
+        <h1>Roulettee!</h1>
 
-      <form onSubmit = {onSubmit}> 
-        <input type="text" value={text} onChange={plusItem}></input>
-        &nbsp;
-        <button> 추가 </button>
-      </form>
+        <form onSubmit={onSubmit}>
+          <input type="text" value={text} onChange={plusItem}></input>
+          &nbsp;
+          <button> 추가 </button>
+        </form>
 
-      <ul>
-        {items.map( item => (
-          <Item {...item} key = {item.id} />
-        ))}
-      </ul>
+        <ul>
+          {items.map((item) => (
+            <Item {...item} key={item.id} />
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <button>돌아!</button>
+      </div>
+
+      <Chart
+        chartType="PieChart"
+        data={[
+          ["투두", "값"],
+          ["test1", 1],
+          ["test2", 1],
+          ["test3", 1],
+          ["test4", 1],
+        ]}
+        options={chartOptions}
+        width="400px"
+        height="400px"
+      />
     </>
   );
 }
 
-function mapStateToProps(state) { //state에서 items를 가져옴
-  return { items: state }
+function mapStateToProps(state) {
+  //state에서 items를 가져옴
+  return { items: state };
 }
 function mapDispatchToProps(dispatch) {
   return {
-      addItem: (text) => dispatch(ADD_ITEM(text)) //App Component에 있는 props인 addItem으로 dispatch
+    addItem: (text) => dispatch(ADD_ITEM(text)), //App Component에 있는 props인 addItem으로 dispatch
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps) (App);
+const CircleDiv = styled.div`
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  background-color: aliceblue;
+  text-align: center;
+  line-height: 400px;
+`;
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
