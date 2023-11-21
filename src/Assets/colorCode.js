45a5e37d9d6f547072758721ae85327607194cbb
Yun-D <yund_272@naver.com>
2023-11-21 22:36:36 +0900

    🎨Design: 체크박스로 사용할 svg 추가 및 적용

--- src/Assets/colorCode.js
+++ src/Assets/colorCode.js
@@ -1,10 +1,12 @@
 export const colorCodes = [
   "#a6e2d9",
   "#379bee",
-  "#d1cdcb",
+  "#d8d6bc",
   "#acabe7",
   "#d8ed7a",
   "#ebaecc",
   "#d5e3e1",
   "#feb0aa",
+  "#C6EAC3",
+  "#F6D9F2",
 ];
--- src/components/Item.js
+++ src/components/Item.js
@@ -2,10 +2,13 @@ import React from "react";
 import { connect } from "react-redux";
 import { DELETE_ITEM } from "../utils/todoSlice";
 
+import { ReactComponent as CheckBox } from "../Assets/unchecked.svg";
+
 function Item({ id, text, onBtnClick }) {
   return (
     <li className="task wrapper">
-      {text}&nbsp;&nbsp;
+      <CheckBox width="20px" height="20px" fill="#acabe7" />
+      &nbsp;&nbsp;{text}&nbsp;&nbsp;
       <button onClick={() => onBtnClick(id)} className="taskBtn">
         삭제
       </button>
